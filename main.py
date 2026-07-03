75ebb56d21d276b1ad304a69d565a23f54a9a6ac
Muns-sourc <munsgit@gmail.com>
2026-07-03 23:08:27 +0300

    Добавления окна загрузки данных, начало создания логики. Рефакторинг интерфейсов

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 import sys 
 from PyQt5  import QtCore, QtGui, QtWidgets
-from Gui2 import *
+from beta3 import *
+from AddWindow import *
 
+
+class AddWindow(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self,parent)
+        self.ui = Ui_Adding()
+        self.ui.setupUi(self)
 
 class MyWin(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
-        QtWidgets.QWidget.__init__(self,parent)
+        QtWidgets.QMainWindow.__init__(self,parent)
         self.ui = Ui_ArtCatalog()
         self.ui.setupUi(self)
+        self.add_window = None
+        self.ui.pushButton.clicked.connect(self.open_add_window)
+    
+    def open_add_window(self):
+        if self.add_window is None:
+            self.add_window = AddWindow()
+            self.add_window.show()
+        else:
+            self.add_window.show()
+        self.add_window.raise_()
+        self.add_window.activateWindow()
 
 
 if __name__ == "__main__":
